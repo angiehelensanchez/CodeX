bc51fb5b28de355a3f281fcc7c9b9961e029cf2f
sofiaramis0 <146663770+sofiaramis0@users.noreply.github.com>
2023-10-16 16:39:19 +0200

    Plantilla Anexo 2

--- src/CodeX/modelo/ListaArticulos.java
+++ src/CodeX/modelo/ListaArticulos.java
@@ -1,4 +1,5 @@
 package CodeX.modelo;
 
-public class ListaArticulos {
+public class ListaArticulos extends Lista<Articulo> {
+// TO-BE-DONE
 }
--- src/CodeX/vista/OnlineStore.java
+++ src/CodeX/vista/OnlineStore.java
@@ -3,5 +3,7 @@ package CodeX.vista;
 public class OnlineStore {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        GestionOS gestion = new GestionOS();
+        gestion.inicio();
     }
 }
