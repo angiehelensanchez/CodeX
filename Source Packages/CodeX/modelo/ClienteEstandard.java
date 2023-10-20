426e081e98648595eaa7e1dbe35c452e6751ad1d
sofiaramis0 <146663770+sofiaramis0@users.noreply.github.com>
2023-10-20 16:00:53 +0200

    articulo ok y cliente en desarrollo

---
+++ Source Packages/CodeX/modelo/ClienteEstandard.java
@@ -0,0 +1,25 @@
+package CodeX.modelo;
+
+public class ClienteEstandard extends Cliente {
+
+    // Este atributo no es necesario al saber ya el tipo por la clase.
+    // private String tipoUsuario;
+    public ClienteEstandard(String nombre, String domicilio, String email, String nif) {
+        super(nombre, domicilio, email, nif);
+    }
+
+    @Override
+    public String tipoCliente() {
+        return "Estandard";
+    }
+
+    @Override
+    public float calcAnual() {
+        return 0;
+    }
+
+    @Override
+    public float descuentoEnv() {
+        return 0;
+    }
+}
