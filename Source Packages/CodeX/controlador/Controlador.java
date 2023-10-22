11eeec000bd6a7d30048878ed497cc98984c1eed
IVAN MB <imartinezbosc@uoc.edu>
2023-10-22 19:31:03 +0200

    yo he creado esta prueba

--- Source Packages/CodeX/controlador/Controlador.java
+++ Source Packages/CodeX/controlador/Controlador.java
@@ -1,11 +1,68 @@
 package CodeX.controlador;
 
-import java.util.List;
-import CodeX.modelo.Datos;
+import java.util.Scanner;
+
 public class Controlador {
-    private Datos datos;
+    private ListaClientes listaClientes;
+    private ListaArticulos listaArticulos;
+    private ListaPedidos listaPedidos;
+
     public Controlador() {
-        datos = new Datos ();
+        listaClientes = new ListaClientes();
+        listaArticulos = new ListaArticulos();
+        listaPedidos = new ListaPedidos();
     }
-// TO-BE-DONE
+
+    public void iniciar() {
+        Scanner scanner = new Scanner(System.in);
+        int opcion;
+
+        do {
+            System.out.println("----- Menú Principal -----");
+            System.out.println("1. Gestionar Clientes");
+            System.out.println("2. Gestionar Artículos");
+            System.out.println("3. Realizar Pedidos");
+            System.out.println("4. Salir");
+            System.out.print("Ingrese su opción: ");
+            opcion = scanner.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    gestionarClientes();
+                    break;
+                case 2:
+                    gestionarArticulos();
+                    break;
+                case 3:
+                    realizarPedidos();
+                    break;
+                case 4:
+                    System.out.println("Saliendo del programa. ¡Hasta luego!");
+                    break;
+                default:
+                    System.out.println("Opción no válida. Intente de nuevo.");
+                    break;
+            }
+        } while (opcion != 4);
+
+        scanner.close();
+    }
+
+    private void gestionarClientes() {
+        // Implementa la lógica para gestionar clientes (agregar, editar, eliminar, listar, etc.).
+    }
+
+    private void gestionarArticulos() {
+        // Implementa la lógica para gestionar artículos (agregar, editar, eliminar, listar, etc.).
+    }
+
+    private void realizarPedidos() {
+        // Implementa la lógica para realizar pedidos.
+    }
+
+    public static void main(String[] args) {
+        Controlador controlador = new Controlador();
+        controlador.iniciar();
+    }
 }
+
