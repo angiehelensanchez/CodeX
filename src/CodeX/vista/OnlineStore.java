72f55be5e69982a9445801e04193bae6ed304e91
Angie Helen Rodas Sánchez <angiehelensanchez@gmail.com>
2023-12-17 20:01:25 +0100

    Delete src directory

--- src/CodeX/vista/OnlineStore.java
+++
@@ -1,9 +0,0 @@
-package CodeX.vista;
-
-public class OnlineStore {
-    public static void main(String[] args) {
-        System.out.println("Iniciando sistema...");
-            GestionOS gestion = new GestionOS();
-        gestion.inicio();
-    }
-}
